ec39185a73381417972679f632dd61328a5c4d19
Ryan Bass <theryanbass@gmail.com>
2022-03-04 12:12:57 -0800

    expand order constructor properties

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -4,15 +4,19 @@ namespace OrderTracker.Models
 {
   public class Order
   {
-    public string Item { get; set; }
+    public string Date { get; set; }
+    public string Title { get; set; }
     public string Price { get; set; } 
+    public string Description { get; set; }
     public int Id { get; }
     public static List<Order> _orders = new List<Order> { };
 
-    public Order(string item, string price)
+    public Order(string date, string title, string price, string description)
     {
-      Item = item;
+      Date = date;
+      Title = title;
       Price = price;
+      Description = description;
       _orders.Add(this);
       Id = _orders.Count;
     }
