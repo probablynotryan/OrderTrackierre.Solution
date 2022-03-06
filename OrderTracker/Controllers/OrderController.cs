3888d15ef1a51d4a57db23445409f9810c44ad66
Ryan Bass <theryanbass@gmail.com>
2022-03-06 12:49:43 -0800

    correct pathing for new order

--- OrderTracker/Controllers/OrderController.cs
+++ OrderTracker/Controllers/OrderController.cs
@@ -7,7 +7,7 @@ namespace OrderTracker.Controllers
     public class OrderController : Controller
     {
 
-      [HttpGet("/vendors/{vendorId}/order/new")]
+      [HttpGet("/vendors/{vendorId}/orders/new")]
       public ActionResult New(int vendorId)
       {
         Vendor vendor = Vendor.Find(vendorId);
