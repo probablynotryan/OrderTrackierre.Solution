ee25aef05a101d1a96190dea0f0e8fafb83b4f4a
Ryan Bass <theryanbass@gmail.com>
2022-03-05 19:20:24 -0800

    add Id Tests / name vendor constructor test

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -7,18 +7,42 @@ using OrderTracker.Models;
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   Order.ClearAll();
-    // }
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
 
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor testVendor = new Vendor("TestWars:ANewTest", "TestWars:TheErrorsStrikeBack");
       Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnName_String()
+    {
+      // Arrange
+      string name = "VendorJanice";
+      Vendor testVendor = new Vendor("VendorJanice", "DescriptionYaddaYadda");
+      // Act
+      string testAgainst = testVendor.Name;
+      // Assert
+      Assert.AreEqual(testAgainst, name);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnVendorId_Int()
+    {
+      // Arrange
+      string name = "Bestie Testie";
+      Vendor testVendor = new Vendor(name, "DescriptionAndMoreWords");
+      // Act
+      int result = testVendor.Id;
+      // Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
