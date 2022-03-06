83f0f74815ce94f2e1eb86a7ccd59e61263b5957
Ryan Bass <theryanbass@gmail.com>
2022-03-05 19:03:14 -0800

    add IDisposable / GetAll Test

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -7,7 +7,7 @@ using OrderTracker.Models;
 namespace OrderTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
     public void Dispose()
     {
@@ -19,6 +19,29 @@ namespace OrderTracker.Tests
     {
       Order testOrder = new Order("TestAndTheFurious", "2Test2Furious", "TestAndTheFurious3", "AnotherTestString");
       Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_String()
+    {
+      // Arrange
+      string price = "Four-fitty";
+      // Act
+      Order testOrder = new Order("DateString", "TitleString", "Four-fitty", "DescriptionString");
+      string result = testOrder.Price;
+      // Asset
+      Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      List<Order> testList = new List<Order> { };
+      // Act
+      List<Order> result = Order.GetAll();
+      // Assert
+      CollectionAssert.AreEqual(testList, result);
     }
   }
 }
